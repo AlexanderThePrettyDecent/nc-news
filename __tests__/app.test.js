61183553d2d1709c31a09bb11fe3f6141cea0477
AlexanderThePrettyDecent <xander.waring@outlook.com>
2024-10-14 12:17:26 +0100

    if succesful implements get  /api/topics to return all topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,32 @@
+const request = require("supertest");
+const db = require("../db/data/test-data");
+const data = require("../db/data/test-data");
+const seed = require("../db/seeds/seed.js");
+const app = require("../app");
+const connection = require("../db/connection.js");
+
+beforeAll(() => {
+  return seed(data);
+});
+afterAll(() => {
+  return connection.end();
+});
+
+describe("api/topics", () => {
+  describe("GET - api/topics", () => {
+    test("200-endpoint responds with 200 status and an array of topic objects", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then((response) => {
+          const topics = response.body.topics;
+          topics.forEach((topic) => {
+            expect(topic).toMatchObject({
+              slug: expect.any(String),
+              description: expect.any(String),
+            });
+          });
+        });
+    });
+  });
+});
