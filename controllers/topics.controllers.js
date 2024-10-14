d7cc6d124eb3330f5e05d4fc52aa55bd010b39d9
AlexanderThePrettyDecent <xander.waring@outlook.com>
2024-10-14 13:50:39 +0100

    added catch block to topic GET controller and added length check to test as per feedback

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,13 @@
 const { selectAllTopics } = require("../models/topics.models");
 
 function getAllTopics(request, response, next) {
-  selectAllTopics().then((results) => {
-    response.status(200).send({ topics: results });
-  });
+  selectAllTopics()
+    .then((results) => {
+      response.status(200).send({ topics: results });
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
 module.exports = { getAllTopics };
